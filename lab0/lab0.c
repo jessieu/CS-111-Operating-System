afed2356e6d04fc768207c9166f21adfdf567aab
Weilan Yu <isweilanyu@gmail.com>
2019-09-30 11:10:28 -0700

    Finished Lab0 Completely

--- lab0/lab0.c
+++ lab0/lab0.c
@@ -1,4 +1,3 @@
-/* Reference: https://linux.die.net/man/3/getopt_long */
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
@@ -6,6 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
 
 #define BUFFER_SIZE 1024
 
@@ -15,31 +15,28 @@ void signal_handler (int sig)
     fprintf(stderr, "Segmentation Fault Caught\n");
     exit(4);
   }
-
 }
 
 void segfault_creator (void)
 {
-  char *ptr = NULL;
-  *ptr = 1;
+  char *str = NULL;
+  *str = 'a';
   return;
 }
 
 int main (int argc, char *argv[])
 {
   int fd;
-  int seg_flg;
-
   char buffer[BUFFER_SIZE];
 
   int opt = 0;
   static struct option long_options[] = {
-            {"input",     required_argument, 0,  'i' },
-            {"output",    required_argument, 0,  'o' },
-            {"segfault",  no_argument,       0,  's' },
-            {"catch",     no_argument,       0,  'c' },
-            {"dump-core", no_argument,       0,  'd' },
-            {0,           0,                 0,   0  }
+    {"input",     required_argument, 0,  'i' },
+    {"output",    required_argument, 0,  'o' },
+    {"segfault",  no_argument,       0,  's' },
+    {"catch",     no_argument,       0,  'c' },
+    {"dump-core", no_argument,       0,  'd' },
+    {0,           0,                 0,   0  }
   };
 
   static char usage[] = "usage: %s [--input=filename] [--output=filename]\
@@ -49,51 +46,40 @@ int main (int argc, char *argv[])
 
   while ((opt = getopt_long(argc, argv, "i:o:scd", long_options, &long_index)) != -1) {
     switch (opt) {
-      case 'i':
-        if ((fd = open(optarg, O_RDONLY, 0)) == -1){
-          fprintf(stderr, "Cannot open file %s.\n", optarg);
-          exit(1);
-        }
-        dup2(fd, 0); /* direct stdin to specific file descriptor */
-        break;
-      case 'o':
-        if ((fd = open (optarg, O_WRONLY|O_CREAT|O_TRUNC, 0)) == -1) {
-          fprintf(stderr, "Cannot create/truncate file %s.\n", optarg);
-          exit(2);
-        }
-        dup2 (fd,1);
-        // if ((fd = open(optarg, O_RDWR, 0)) == -1){
-        //   if ((fd = create (optarg, PERMS)) == -1) {
-        //     fprintf(stderr, "Cannot create file %s.\n", optarg);
-        //     exit(2);
-        //   }
-        //   dup2(fd, 1);
-        // }else {
-        //   if (ftruncate (fd, 0) == -1){
-        //     fprintf(stderr, "Cannot truncate file %s.\n", optarg);
-        //     exit(2);
-        //   }
-        //   dup2(fd, 1);
-        // }
-        break;
-      case 's':
-        //memset(NULL, 1, 1);
-        //raise(SIGSEGV);
-        seg_flg = 1;
-        break;
-      case 'c':
-        signal (SIGSEGV, signal_handler);
-        break;
-      case 'd': /* record the state of working memory when crashed */
-        abort();
-        break;
-      default:
-        fprintf(stderr, "Cannot identify option.\n%s\n", usage);
-        exit(3);
+    case 'i':
+      if ((fd = open(optarg, O_RDONLY, 0)) == -1){
+	fprintf(stderr, "Error on argument --input. Unable to open input file %s.\n", optarg);
+	exit(2);
+      }
+      dup2(fd, 0); /* direct stdin to input file */
+      close(fd);
+      break;
+    case 'o':
+      if ((fd = open (optarg, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR | S_IWUSR)) == -1) {
+	fprintf(stderr, "Error on argument --output. Unable to open output file %s.\n", optarg);
+	exit(3);
+      }
+      dup2 (fd,1);
+      close(fd);
+      break;
+    case 's':
+      segfault_creator();
+      break;
+    case 'c': /* catch sigsegv and handle immediately */
+      signal (SIGSEGV, signal_handler);
+      break;
+    case 'd': 
+      /* Revert to default handler, will exit on next SIGSEGV with core dump */
+      signal(SIGSEGV, SIG_DFL);
+      break;
+    default:
+      fprintf(stderr, "Unrecoginzed argument.\n%s\n", strerror(errno));
+      printf(usage);
+      exit(1);
     }
-
-    if (seg_flg) segfault_creator();
   }
+  
+  /* Read input and then write to output */
   int n;
   while ((n = read(0, buffer, BUFFER_SIZE)) > 0) {
     if (write (1, buffer, n) < 0) {
@@ -101,8 +87,10 @@ int main (int argc, char *argv[])
       exit (3);
     }
   }
+  if (n < 0) {
+    fprintf (stderr, "Error on reading file: %s\n", strerror (errno));
+    exit(2);
+  }
 
-  close (0);
-  close (1);
-  return 0;
+  exit(0);
 }
